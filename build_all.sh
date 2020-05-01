c54345810a33ce946835dc04baab888ec044f3aa
Angel Abad <angelabad@gmail.com>
2020-05-01 12:34:57 +0200

    fix(resume): Remove build_pdf step from build commands

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@
 cd curriculum
 # run hugo to create pdf from updated content
 hugo
-./build_pdf.sh
+#./build_pdf.sh
 # run hugo again to install new created pdf
 hugo
 cd -
@@ -14,3 +14,7 @@ cd landing
 hugo
 cd -
 
+## NOTICE
+echo "********************************************"
+echo "* REMEMBER TO BUILD YOUR PDF BEFORE UPLOAD *"
+echo "********************************************"
